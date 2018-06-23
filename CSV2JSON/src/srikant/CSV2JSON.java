5a680b932c7a9e53d538c05a23bfbc86bcdccb7d
srikant0027 <30918918+srikant0027@users.noreply.github.com>
2018-06-23 15:06:02 +0530

    Add files via upload

---
+++ CSV2JSON/src/srikant/CSV2JSON.java
@@ -0,0 +1,33 @@
+package srikant;
+
+import java.io.IOException;
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+ 
+import com.fasterxml.jackson.databind.MappingIterator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+ 
+public class CSV2JSON {
+ 
+    public static void main(String[] args) throws Exception {
+        File input = new File("src\\srikant\\input1.csv");
+        File output = new File("src\\srikant\\abc\\output.json");
+ 
+        CsvSchema csvSchema = CsvSchema.builder().setUseHeader(true).build();
+        CsvMapper csvMapper = new CsvMapper();
+ 
+        // Read data from CSV file
+        List<Object> readAll = csvMapper.readerFor(Map.class).with(csvSchema).readValues(input).readAll();
+ 
+        ObjectMapper mapper = new ObjectMapper();
+ 
+        // Write JSON formated data to output.json file
+        mapper.writerWithDefaultPrettyPrinter().writeValue(output, readAll);
+ 
+        // Write JSON formated data to stdout
+        System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(readAll));
+    }
+}
